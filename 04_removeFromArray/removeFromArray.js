c6bb0cc3255dfc375b1c72255b8efaa64e55ae3f
Ryan Le <kiesagent@gmail.com>
2023-01-05 17:29:02 -0500

    Finished the required exercises for now

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    args.forEach((element) => {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === element) {
+                array.splice(i, 1);
+            }
+        }
+    });
 
+    return array;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let result = 0;
+
+  result = (x - 32) * (5/9);
+
+  return Math.round(result * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  let result = 0;
+
+  result = (x * (9/5) + 32);
+
+  return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
